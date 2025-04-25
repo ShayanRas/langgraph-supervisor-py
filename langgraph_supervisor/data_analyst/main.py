c3f72e0cb4d24a3f6a08f29b6634251d32c11c1e
ShayanRas <84932824+ShayanRas@users.noreply.github.com>
2025-04-25 10:08:06 -0400

    .

--- langgraph_supervisor/data_analyst/main.py
+++ langgraph_supervisor/data_analyst/main.py
@@ -1,12 +1,12 @@
-from langgraph_supervisor.data_engineer.agent_tools.alpha_vantage.get_econ_data import get_econ_data
-from langgraph_supervisor.data_engineer.agent_tools.twelve_data.get_time_series import get_time_series
-from langgraph_supervisor.data_engineer.agent_tools.alpha_vantage.get_commodities_timeseries import get_commodity_data
-from langgraph_supervisor.data_engineer.agent_tools.alpha_vantage.get_news_sentiment import get_news_sentiment
+from langgraph_supervisor.data_analyst.agent_tools.alpha_vantage.get_econ_data import get_econ_data
+from langgraph_supervisor.data_analyst.agent_tools.twelve_data.get_time_series import get_time_series
+from langgraph_supervisor.data_analyst.agent_tools.alpha_vantage.get_commodities_timeseries import get_commodity_data
+from langgraph_supervisor.data_analyst.agent_tools.alpha_vantage.get_news_sentiment import get_news_sentiment
 from langgraph.prebuilt import create_react_agent
-from langgraph_supervisor.data_engineer.prompt import prompt
+from langgraph_supervisor.data_analyst.prompt import prompt
 from langgraph_supervisor.database_tools.db_tools import execute_sql
 from langchain_openai import ChatOpenAI
-from langgraph_supervisor.data_engineer.agent_tools.E2B.main import E2BCodeInterpreterTool
+from langgraph_supervisor.data_analyst.agent_tools.E2B.main import E2BCodeInterpreterTool
 import os
 import dotenv
 
@@ -16,14 +16,14 @@ llm = ChatOpenAI(model="gpt-4.1", api_key=os.getenv("OPENAI_API_KEY"), temperatu
 
 code_interpreter = E2BCodeInterpreterTool()
 
-data_engineer = create_react_agent(
+data_analyst = create_react_agent(
     model=llm,
     tools=[get_econ_data, 
-    get_time_series, 
+    get_time_series,
     execute_sql, 
     get_commodity_data, 
     get_news_sentiment,
     code_interpreter],
-    name="data_engineer",
+    name="data_analyst",
     prompt=prompt
 )
